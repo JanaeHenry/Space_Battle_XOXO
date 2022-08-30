bb1fe6b76038ed4331059e15e778b37cae23e781
Janae Henry <janaedhenry@gmail.com>
2022-08-30 18:47:01 -0400

    changes made

--- script.js
+++ script.js
@@ -24,7 +24,10 @@ logic for generating alien health/stats
 
 */
 
-class Ship {
+ //create attack button that is displayed with game rules after player presses start button
+      //create a method that runs whenever the attack button is clicked
+
+class Ship { //Make A Ship Class 
     constructor(name, hull, firepower, accuracy) {
       this.name = name,
       this.hull = hull ,
@@ -33,7 +36,7 @@ class Ship {
     }
   }
 
-class UssAssembly extends Ship {
+class HumanShip extends Ship { //Make the Human Ship sub-class.
     constructor(name, hull, firepower, accuracy) {
       super(name, hull, firepower, accuracy)
       
@@ -42,42 +45,152 @@ class UssAssembly extends Ship {
         this.firepower = 5,
         this.accuracy = .7
     }
-  attackAlien1(target) {
-    return AlienShip1.hull -= 2;
-    
-  } 
 
-  hitAccuracy() {
-    return 
   }
+    // Make a method for the USS Assembly that will attack a given target. 
+    // The target can be an input to the method.
+    // Run the method and pass it the alien ship.
+    // Make it so the method reduces the target's hull by the firepower of the USS Assembly.
+    
+    
 
-   
-}
+    const btn = document.getElementsByClassName('btn');
+    const startButton = document.getElementById('start');
+    const startTitle = document.getElementById('title');
+    const attackButton = document.getElementById('attack');
+    //Hide start button after it is clicked
+    startButton.addEventListener('click', () => {
+      startButton.style.visibility = 'hidden';
+      startTitle.style.visibility = 'hidden';
+      attackButton.style.visibility = 'hidden';
+    })
 
-class AlienShip1 extends Ship {
+
+
+
+
+    //Start Game function
+      const startGame = () => {
+      document.getElementsByClassName('start-game');
+    }
+
+    //Attack button
+    attackButton.addEventListener('click', () => {
+      // check if any enemy is left (if statement -- not a loop)
+      if(alienGroup.length !== 0) {
+        // if so, attack the first one in the array
+        ussAssembly.attackAlien(alienGroup)
+
+      } else {
+        // else, end the game with disabling button and popping up YOU WIN! alert.
+        endGame();
+
+      }
+          
+    })
+
+
+    //Attack function
+    const fireAttack = () => {
+      //insert attackAlien attack method here... you know what I mean
+    }
+    
+    //Attack method
+    // attackAlien(target) {
+    //   let generateAccuracy = Math.random();
+    //   while(this.hull > 0){
+    //     fireAttack();
+    //   }
+    // }
+
+    //End Game function
+    const endGame = () => {
+      window.alert('You\'ve Defeated all the Aliens!');
+      document.addEventListener('click', startGame)
+
+    }
+  
+  
+
+class AlienShip extends Ship { //Make an Alien Ship sub-class.
     constructor(name, hull, firepower, accuracy){
       super(name, hull, firepower, accuracy)
         this.hull = Math.floor(Math.random() * ((6 - 3) + 1) + 3); // Finds random number between 3 and 6
         this.firepower = Math.floor(Math.random() * ((4 - 2) + 1) + 2); // Finds random number between 2 and 4 
         this.accuracy = Math.random() * (.8 - .6) + .6;
     }
-    attackUss() {
-        return UssAssembly.hull -= 2;
-    }
+    
 }
 
-let startMessage = window.prompt('Press START to begin');
+//Make an instance of each class:
+const ussAssembly = new HumanShip;
+const alienShip1 = new AlienShip('Durbozi');
+const alienShip2 = new AlienShip('Vimocadon');
+const alienShip3 = new AlienShip('Alimotton');
+const alienShip4 = new AlienShip('Qitago');
+const alienShip5 = new AlienShip('Kumon');
+const alienShip6 = new AlienShip('Zeeta');
 
-//insert event listener here to start game when button is clicked
-function clickHandler(event) {
-    console.log('Start Button Clicked');
- }
+const alienGroup = [alienShip1 , alienShip2, alienShip3, alienShip4 , alienShip5, alienShip6]
 
-const btn = document.querySelector('.btn');
-btn.addEventListener('click', clickHandler);
 
-if(AlienShip1.hull <= 0) {
-    window.prompt('You have defeated the enemy!') 
-} else {
-    this.attackAlien();
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let startMessage = window.prompt('Press START to begin');
+
+// //insert event listener here to start game when button is clicked
+// function clickHandler(event) {
+//     console.log('Start Button Clicked');
+//  }
+
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click', clickHandler);
+
+// if(AlienShip1.hull <= 0) {
+//     window.prompt('You have defeated the enemy!') 
+// } else {
+//     this.attackAlien();
+// }
+
+
+
+
+
+
+
+
+
+
+
+// attackAlien1(target) {
+//   return AlienShip1.hull -= 2;
+  
+// } 
+
+// hitAccuracy() {
+//   return 
+// }
+
+// attackUss() {
+//   return UssAssembly.hull -= 2;
+// }
