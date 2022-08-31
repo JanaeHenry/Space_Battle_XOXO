88dae7e7462c555af191a198dfd355e9b1f0c6f2
Janae Henry <janaedhenry@gmail.com>
2022-08-30 23:06:15 -0400

    additional changes made

--- script.js
+++ script.js
@@ -45,24 +45,84 @@ class HumanShip extends Ship { //Make the Human Ship sub-class.
         this.firepower = 5,
         this.accuracy = .7
     }
+    attackAlien(target){ // attackAlien occurs whenever the player attacks the alien. -- attackMain (on the alien class) occurs whenever the alien attacks the player.
+      let randomNum = Math.random()
+      if (randomNum <= this.accuracy) { // checks if the player hits the alien
+        target.hull -= this.firepower // subtracts the player's firepower from the enemy's health if it hits
+          window.alert(`Attacked ${target.name} for ${this.firepower} damage!`)
+      }
+      else {
+          (window.alert('You have missed!')) // if it doesn't hit, do something here
+      }
+      // after the player attacks, we are going to run the target's (alien's) attackMain method so that it attacks back.
+      target.attackMain(this);
+      if(target.hull <= 0){
+        alienGroup.shift();
+      }
+      if (alienGroup.length === 0){
+        window.alert('You\'ve Defeated all the Aliens!');
+      }
+  }
 
+    // defeatEnemy(target){
+    //   if( target.hull > 0) {
+    //     ussAssembly.attackAlien(alienGroup[0]);
+    //   }
+    // }
+    
   }
     // Make a method for the USS Assembly that will attack a given target. 
     // The target can be an input to the method.
     // Run the method and pass it the alien ship.
     // Make it so the method reduces the target's hull by the firepower of the USS Assembly.
     
-    
+  
 
-    const btn = document.getElementsByClassName('btn');
+class AlienShip extends Ship { //Make an Alien Ship sub-class.
+    constructor(name, hull, firepower, accuracy){
+      super(name, hull, firepower, accuracy)
+      this.name = name,
+        this.hull = Math.floor(Math.random() * ((6 - 3) + 1) + 3), // Finds random number between 3 and 6
+        this.firepower = Math.floor(Math.random() * ((4 - 2) + 1) + 2), // Finds random number between 2 and 4 
+        this.accuracy = Math.random() * ((.8 - .6) + .6)
+    }
+    attackMain(target) {
+      let randomNum = Math.random()
+       if (randomNum < this.accuracy) {
+          target.hull -= this.firepower
+          window.alert("You've been hit"); // target is the actual target of the attack. any changes made on target.whatever are going to affect the actual thing given to it.
+           // (to reduce alien's health, think about target.hull)
+       }
+       else { window.alert('Enemy missed!') }
+      }
+    
+}
+
+//Make an instance of each class:
+const ussAssembly = new HumanShip;
+const alienShip1 = new AlienShip('Durbozi');
+const alienShip2 = new AlienShip('Vimocadon');
+const alienShip3 = new AlienShip('Alimotton');
+const alienShip4 = new AlienShip('Qitago');
+const alienShip5 = new AlienShip('Kumon');
+const alienShip6 = new AlienShip('Zeeta');
+
+const alienGroup = [alienShip1 , alienShip2, alienShip3, alienShip4 , alienShip5, alienShip6]
+
+const btn = document.getElementsByClassName('btn');
     const startButton = document.getElementById('start');
     const startTitle = document.getElementById('title');
     const attackButton = document.getElementById('attack');
+
+    window.onload = () => {
+      document.getElementById('attack').style.visibility = 'hidden';
+      document.getElementById('restart').style.visibility = 'hidden';
+    }
     //Hide start button after it is clicked
     startButton.addEventListener('click', () => {
       startButton.style.visibility = 'hidden';
       startTitle.style.visibility = 'hidden';
-      attackButton.style.visibility = 'hidden';
+      attackButton.style.visibility = 'visible';
     })
 
 
@@ -90,11 +150,7 @@ class HumanShip extends Ship { //Make the Human Ship sub-class.
     })
 
 
-    //Attack function
-    const fireAttack = () => {
-      //insert attackAlien attack method here... you know what I mean
-    }
-    
+     
     //Attack method
     // attackAlien(target) {
     //   let generateAccuracy = Math.random();
@@ -110,30 +166,6 @@ class HumanShip extends Ship { //Make the Human Ship sub-class.
 
     }
   
-  
-
-class AlienShip extends Ship { //Make an Alien Ship sub-class.
-    constructor(name, hull, firepower, accuracy){
-      super(name, hull, firepower, accuracy)
-        this.hull = Math.floor(Math.random() * ((6 - 3) + 1) + 3); // Finds random number between 3 and 6
-        this.firepower = Math.floor(Math.random() * ((4 - 2) + 1) + 2); // Finds random number between 2 and 4 
-        this.accuracy = Math.random() * (.8 - .6) + .6;
-    }
-    
-}
-
-//Make an instance of each class:
-const ussAssembly = new HumanShip;
-const alienShip1 = new AlienShip('Durbozi');
-const alienShip2 = new AlienShip('Vimocadon');
-const alienShip3 = new AlienShip('Alimotton');
-const alienShip4 = new AlienShip('Qitago');
-const alienShip5 = new AlienShip('Kumon');
-const alienShip6 = new AlienShip('Zeeta');
-
-const alienGroup = [alienShip1 , alienShip2, alienShip3, alienShip4 , alienShip5, alienShip6]
-
-
 
 
 
